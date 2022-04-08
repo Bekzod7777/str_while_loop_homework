8922a1cac23120dc9ceff582b328eec2134dd258
Bekzod <bekzodmirsaidov91@gmail.com>
2022-04-08 16:36:32 +0500

    10m

--- While10.py
+++ While10.py
@@ -1,4 +1,13 @@
 def main(s):
+    s=int(s)
+    t=0
+    i=0
+    while s>0:
+
+        n=s%10
+        s=s//10
+        if n%2==1:
+            t +=n
     """
     A string of numbers is given. Find and return the sum of all odd numbers.
     Args:
@@ -6,4 +15,4 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    return  t
