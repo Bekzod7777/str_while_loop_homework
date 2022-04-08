28c08ac5477c5573cbc3bb885ebf2855fc3a9e87
Bekzod <bekzodmirsaidov91@gmail.com>
2022-04-08 16:25:55 +0500

    8m

--- While08.py
+++ While08.py
@@ -1,4 +1,12 @@
 def main(s):
+    s=int(s)
+    t=0
+    i=0
+    while s>0:
+        n=s%10
+        s=s//10
+        if n%2==1:
+            i+=1
     """
     A string of numbers is given. Find how many odd numbers there are and return.
     Args:
@@ -6,4 +14,5 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    return i
+    
