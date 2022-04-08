f31296a3e38def74de49e1c32792c7a7606299bd
Bekzod <bekzodmirsaidov91@gmail.com>
2022-04-08 16:21:42 +0500

    7m

--- While07.py
+++ While07.py
@@ -1,9 +1,18 @@
 def main(s):
+    t=0
+    i=0
+    while s>0:
+        n=s%10
+        s=s//10
+        if n%2==0:
+            i+=1
+
+        
     """
     A string of numbers is given. Find how many even numbers there are and return.
     Args:
         s: str
     Returns:
         int: return answer
-    """
-    return
+    """   
+    return  i
