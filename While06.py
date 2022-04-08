e1564fcb226ad5dbef522c1ed73a5234eeefedb4
Bekzod <bekzodmirsaidov91@gmail.com>
2022-04-08 15:59:47 +0500

    6m

--- While06.py
+++ While06.py
@@ -1,4 +1,11 @@
 def main(s):
+    unli=['A','O','E','I','U','a','o','e','i','u']
+    t=0
+    i=0
+    while len(s)>t:
+        if(s[t] not in unli)  and s[t].isalpha():
+            i +=1
+        t +=1
     """
     A variable of type str is given. Find and return how many consonant letters there are.
     Args:
@@ -7,4 +14,4 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    return i
