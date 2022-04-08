131bd5d0b2912633c2b09b1840c0083c4ee113cd
Bekzod <bekzodmirsaidov91@gmail.com>
2022-04-08 15:15:33 +0500

    3m

--- While03.py
+++ While03.py
@@ -1,4 +1,11 @@
 def main(s):
+    
+    i=0
+    t=0
+    while len(s)>t:
+        if s[t].ispunctuation()==True:
+            i +=1
+        t +=1
     """
     A variable of type str is given. Find how many punctuations it contains and return.
     Args:
@@ -6,4 +13,5 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    return t
+    
