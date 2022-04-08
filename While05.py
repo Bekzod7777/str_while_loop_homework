4e77fb379a8cbf0a36ab7afa106091d5733c7adb
Bekzod <bekzodmirsaidov91@gmail.com>
2022-04-08 15:46:05 +0500

    5m

--- While05.py
+++ While05.py
@@ -1,4 +1,10 @@
 def main(s):
+    i=0
+    t=0
+    while len(s)>t:
+        if s[t]==s[t].lower():
+            i +=1
+        t +=1
     """
     A variable of type str is given. Find how many lowercase letters there are and return.
     Args:
@@ -6,4 +12,6 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+
+    return i
+    
